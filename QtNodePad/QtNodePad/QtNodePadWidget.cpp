03bda993752f642b07e2811d1b86b88664df3e5d
shaw <1939484341@qq.com>
2022-05-09 11:44:03 +0800

    读取和保存

--- QtNodePad/QtNodePad/QtNodePadWidget.cpp
+++ QtNodePad/QtNodePad/QtNodePadWidget.cpp
@@ -1,8 +1,12 @@
 #include "QtNodePadWidget.h"
 #include <QFileIconProvider>
+#include <QMessageBox>
+#include <QFileDialog>
+#include <QTextStream>
 
 QtNodePad::QtNodePad(QWidget *parent)
     : QMainWindow(parent)
+	, m_settings("NewNotePad")
 {
     ui.setupUi(this);
 
@@ -26,4 +30,61 @@ QtNodePad::QtNodePad(QWidget *parent)
 QtNodePad::~QtNodePad()
 {
 
+}
+
+void QtNodePad::openFile(const QString & path)
+{
+
+}
+
+bool QtNodePad::isModified() const
+{
+	return ui.mainTextEdit->toPlainText() != savedContent;
+}
+
+void QtNodePad::updateWindowTitle()
+{
+	this->setWindowTitle((isModified() ? "*" : "") + fileName + " - 记事本");
+}
+
+bool QtNodePad::askSaveFile()
+{
+	if (!isModified())
+		return true;
+
+	// 有未保存的更改
+	int btn = QMessageBox::question(this, "记事本", "你想更改保存到 " + (fileName.isEmpty() ? "无标题" : fileName) + " 吗？", "保存(&S)", "不保存(&N)", "取消");
+	if (btn == 2) // 取消
+		return false;
+	if (btn == 0) // 保存
+	{
+		return sltActionSaveFile();
+	}
+	return true;
+}
+
+bool QtNodePad::sltActionSaveFile()
+{
+	if (filePath.isEmpty()) // 没有路径，另存为
+	{
+		QString recentPath = m_settings.value("recent/filePath").toString();
+		QString path = QFileDialog::getSaveFileName(this, "另存为", recentPath, "*.txt");
+		if (path.isEmpty())
+			return false;
+		m_settings.setValue("recent/filePath", path);
+		filePath = path;
+		fileName = QFileInfo(path).baseName();
+	}
+
+	// 写出文件
+	QFile file(filePath);
+	if (!file.open(QIODevice::WriteOnly))
+		return false;
+	QTextStream ts(&file);
+	ts.setCodec("GBK");
+	savedContent = ui.mainTextEdit->toPlainText();
+	ts << savedContent;
+	file.close();
+	updateWindowTitle();
+	return true;
 }
--- QtNodePad/QtNodePad/QtNodePadWidget.h
+++ QtNodePad/QtNodePad/QtNodePadWidget.h
@@ -4,6 +4,7 @@
 
 #include <QtWidgets/QMainWindow>
 #include <QLabel>
+#include <QSettings>
 #include "ui_QtNodePadWidget.h"
 
 class QtNodePad : public QMainWindow
@@ -14,8 +15,28 @@ public:
     QtNodePad(QWidget *parent = Q_NULLPTR);
 	~QtNodePad();
 
+	//打开文件或者创建空文件
+	void openFile(const QString& path);
+
+	//是否保存
+	bool isModified() const;
+
+private:
+	void updateWindowTitle();
+	bool askSaveFile();
+
+private slots:
+	bool sltActionSaveFile();
+
 private:
     Ui::QtNodePadClass ui;
+	QSettings m_settings;
+
+	//文件属性
+	QString filePath;
+	QString fileName;
+	QString savedContent;
+	int zoomSize = 100;
 
 	//底部工具化文本控件
 	QLabel* m_posLabel = nullptr;
