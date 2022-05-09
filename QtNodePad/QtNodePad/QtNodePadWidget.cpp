2b518f7f351a7c8a2a293f3f9a59350e5186432e
shaw <1939484341@qq.com>
2022-05-09 11:29:04 +0800

    底部工具栏初始化

--- QtNodePad/QtNodePad/QtNodePadWidget.cpp
+++ QtNodePad/QtNodePad/QtNodePadWidget.cpp
@@ -6,6 +6,17 @@ QtNodePad::QtNodePad(QWidget *parent)
 {
     ui.setupUi(this);
 
+	//底部工具化文本控件初始化
+	ui.statusBar->addPermanentWidget(new QLabel(this), 6);
+	m_posLabel = new QLabel("第 1 行，第 1 列", this);
+	ui.statusBar->addPermanentWidget(m_posLabel, 3);
+	m_zoomLabel = new QLabel("100%", this);
+	ui.statusBar->addPermanentWidget(m_zoomLabel, 1);
+	m_lineLabel = new QLabel("Windows (CRLF)", this);
+	ui.statusBar->addPermanentWidget(m_lineLabel, 3);
+	m_codecLabel = new QLabel("GBK", this);
+	ui.statusBar->addPermanentWidget(m_codecLabel, 1);
+
 	// 使用window系统本身笔记本icon
 	QFileIconProvider ip;
 	QIcon icon = ip.icon(QFileInfo("C:\\Windows\\System32\\notepad.exe"));
--- QtNodePad/QtNodePad/QtNodePadWidget.h
+++ QtNodePad/QtNodePad/QtNodePadWidget.h
@@ -3,6 +3,7 @@
 #pragma execution_character_set("utf-8")
 
 #include <QtWidgets/QMainWindow>
+#include <QLabel>
 #include "ui_QtNodePadWidget.h"
 
 class QtNodePad : public QMainWindow
@@ -15,4 +16,10 @@ public:
 
 private:
     Ui::QtNodePadClass ui;
+
+	//底部工具化文本控件
+	QLabel* m_posLabel = nullptr;
+	QLabel* m_zoomLabel = nullptr;
+	QLabel* m_lineLabel = nullptr;
+	QLabel* m_codecLabel = nullptr;
 };
